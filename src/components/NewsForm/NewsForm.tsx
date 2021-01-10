c68a176f9fdddf35635efe5fc906ac634356d654
Aleksey Palanevich <palanevich.aleksey@gmail.com>
2021-01-10 13:42:40 +0300

    feat: NewsForm

--- src/components/NewsForm/NewsForm.tsx
+++ src/components/NewsForm/NewsForm.tsx
@@ -1,11 +1,13 @@
-import { Input } from 'antd'
+import { Button, Form, Input, Select } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { newsSelectors } from '../../features/news'
 import {
+  actions,
   postNewsItemThunk,
   updateNewsItemThunk
 } from '../../features/news/newsReducer'
+import css from './NewsForm.module.css'
 
 const NewsForm: React.FC = () => {
   const dispatch = useDispatch()
@@ -20,10 +22,11 @@ const NewsForm: React.FC = () => {
   const [tags, setTags] = useState(currentNewsItem?.tags)
 
   useEffect(() => {
-    setDate(currentNewsItem?.date)
-    setTitle(currentNewsItem?.title)
-    setImage(currentNewsItem?.image)
-    setMessage(currentNewsItem?.message)
+    setDate(currentNewsItem.date)
+    setTitle(currentNewsItem.title)
+    setImage(currentNewsItem.image)
+    setMessage(currentNewsItem.message)
+    setTags(currentNewsItem.tags)
   }, [currentNewsItem])
 
   const handleChangeTitle = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -32,8 +35,14 @@ const NewsForm: React.FC = () => {
   const handleChangeMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.currentTarget.value)
   }
-  const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeImage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setImage(e.currentTarget.value)
+  }
+  const handleSelectTag = (value: string) => {
+    setTags([...tags, value])
+  }
+  const handleDeselectTag = (value: string) => {
+    setTags(tags.filter((t) => t !== value))
   }
 
   const handlePostNews = () => {
@@ -62,52 +71,99 @@ const NewsForm: React.FC = () => {
       })
     )
   }
+  const handleHideForm = () => {
+    dispatch(actions.toggleAppWorkMode('view'))
+    dispatch(actions.clearCurrentNewsItem())
+  }
+
+  const layout = {
+    labelCol: {
+      span: 4,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  }
+  const tailLayout = {
+    wrapperCol: {
+      offset: 4,
+      span: 16,
+    },
+  }
 
   return (
-    <div className="newsForm">
-      <Input.TextArea
-        // type="text"
-        // className="newsForm__title"
-        value={title}
-        onChange={handleChangeTitle}
-        placeholder="Title"
-        allowClear
-        autoSize={{ minRows: 1, maxRows: 2 }}
-      />
-      <Input.TextArea
-        // type="text"
-        // className="newsForm__title"
-        value={message}
-        onChange={handleChangeMessage}
-        placeholder="Message"
-        allowClear
-        autoSize={{ minRows: 2, maxRows: 4 }}
-      />
+    <div className={css.newsForm}>
+      <Form {...layout}>
+        <Form.Item
+          label="Title"
+          rules={[
+            {
+              required: true,
+              message: 'Please input title!',
+            },
+          ]}
+        >
+          <Input.TextArea
+            value={title}
+            onChange={handleChangeTitle}
+            autoFocus
+            allowClear
+            autoSize={{ minRows: 1, maxRows: 2 }}
+          />
+        </Form.Item>
+        <Form.Item
+          label="Message"
+          rules={[
+            {
+              required: true,
+              message: 'Please input news!',
+            },
+          ]}
+        >
+          <Input.TextArea
+            value={message}
+            onChange={handleChangeMessage}
+            allowClear
+            autoSize={{ minRows: 3, maxRows: 6 }}
+          />
+        </Form.Item>
+        <Form.Item label="Image URL">
+          <Input.TextArea
+            size="large"
+            value={image}
+            onChange={handleChangeImage}
+            allowClear
+            autoSize={{ maxRows: 2 }}
+          />
+        </Form.Item>
+        <Form.Item label="Tags">
+          <Select
+            mode="tags"
+            defaultValue={tags}
+            open={false}
+            onSelect={handleSelectTag}
+            onDeselect={handleDeselectTag}
+          />
+        </Form.Item>
 
-      {/* <input
-        type="text"
-        className="newsForm__message"
-        value={message}
-        onChange={handleChangeMessage}
-        placeholder="Text"
-      />
-      <input
-        type="text"
-        className="newsForm__image"
-        value={image}
-        onChange={handleChangeImage}
-        placeholder="Image URL"
-      /> */}
-      {appWorkMode === 'add' && (
-        <button type="button" onClick={handlePostNews}>
-          Post News
-        </button>
-      )}
-      {appWorkMode === 'edit' && (
-        <button type="button" onClick={handleUpdateNews}>
-          Update News
-        </button>
-      )}
+        <Form.Item {...tailLayout}>
+          <span className={css['newsForm__submitBtn-container']}>
+            {appWorkMode === 'add' && (
+              <Button type="primary" onClick={handlePostNews}>
+                Post News
+              </Button>
+            )}
+            {appWorkMode === 'edit' && (
+              <Button type="primary" onClick={handleUpdateNews}>
+                Update News
+              </Button>
+            )}
+          </span>
+          <Button type="default" danger onClick={handleHideForm}>
+            Cancel
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
